4c9c12be3db8a4feda4c18bcdfcd70a6385dc777
Felipe Côrtes <contatos.cortes@gmail.com>
2020-06-02 15:26:59 -0300

    info license updated

--- pkg/deploy/kong_plugin.go
+++ pkg/deploy/kong_plugin.go
@@ -1,3 +1,5 @@
+// Copyright © 2020 Vertigo Tecnologia. All rights reserved.
+// Licensed under the  Apache License, Version 2.0. See LICENSE file in the project root for full license information.
 package deploy
 
 import y "gopkg.in/yaml.v2"
